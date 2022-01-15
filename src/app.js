1400c9aa163b40846f16d4440c199785b5864489
Mohan <88998744+mohanhu@users.noreply.github.com>
2022-01-15 09:28:06 +0530

    first

---
+++ src/app.js
@@ -0,0 +1,21 @@
+let express = require( 'express' );
+let app = express();
+
+var port = process.env.PORT || 3000;
+let server = require( 'http' ).Server( app );
+let io = require( 'socket.io' )( server );
+let stream = require( './ws/stream' );
+let path = require( 'path' );
+let favicon = require( 'serve-favicon' );
+
+app.use( favicon( path.join( __dirname, 'favicon.ico' ) ) );
+app.use( '/assets', express.static( path.join( __dirname, 'assets' ) ) );
+
+app.get( '/', ( req, res ) => {
+    res.sendFile( __dirname + '/index.html' );
+} );
+
+
+io.of( '/stream' ).on( 'connection', stream );
+
+server.listen(port,()=> console.log("server is running on "+port));
